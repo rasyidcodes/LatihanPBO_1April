26f369f93303361d436d5ad22a50ab313871e929
abyan <abyanyusuf07@gmail.com>
2023-05-23 15:52:41 +0700

    Nomor 5b

--- bangun3D/Kerucut.java
+++ bangun3D/Kerucut.java
@@ -1,4 +1,22 @@
 package bangun3D;
 
-public class Kerucut {
+public class Kerucut extends Tabung{
+    private double phi = 22.0/7;
+    public Kerucut(double r, double tinggi) {
+        super(r, tinggi);
+    }
+
+    public double getLuas() {
+        return getLuasAlas() + phi*getR () * getSisiMiring();
+    }
+
+    public double getSisiMiring() {
+        double s = getR() * getR() + getTinggi () *getTinggi();
+        return Math.sqrt(s);
+    }
+
+    public double getVolume() {
+        return super. getVolume() / 3;
+    }
+
 }
