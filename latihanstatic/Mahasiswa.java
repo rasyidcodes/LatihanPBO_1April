49fa91ff98ec0ca2a829708098f4fd312d33d22e
Darriel2603 <darrielmarkerizal@gmail.com>
2023-05-23 15:33:50 +0700

    darel

---
+++ latihanstatic/Mahasiswa.java
@@ -0,0 +1,11 @@
+package latihanstatic;
+
+public class Mahasiswa {
+    public static String prodi = "TRPL";
+    public String nama = "mr X";
+
+    public static void main(String[] args) {
+        System.out.println(Mahasiswa.prodi);
+        System.out.println(Mahasiswa.nama);
+    }
+}
