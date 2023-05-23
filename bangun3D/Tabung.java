00cb57d5a0bf3564077d8ea892b39dac0f0939f9
abyan <abyanyusuf07@gmail.com>
2023-05-23 15:31:24 +0700

    Nomor 5b

--- bangun3D/Tabung.java
+++ bangun3D/Tabung.java
@@ -1,4 +1,48 @@
 package bangun3D;
 
 public class Tabung {
+    private double phi = 22.0/7;
+    private double r;
+    private double tinggi;
+
+    public Tabung(double r, double tinggi) {
+        this.r = r;
+        this.tinggi = tinggi;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    public double getLuas() {
+        return 2 * getLuasAlas() + 2 * phi * r * tinggi;
+    }
+
+    public double getLuasAlas (){
+        return phi*r*r;
+    }
+
+    public double getVolume () {
+        return phi * r * r * tinggi;
+    }
+
+    @Override
+    public String toString() {
+        return "Tabung{" +
+                "r=" + r +
+                ", tinggi=" + tinggi +
+                '}';
+    }
 }
