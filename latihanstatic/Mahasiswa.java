7fe39504eb7d5dccf9a5fac4bac8057cba5217a8
Darriel2603 <darrielmarkerizal@gmail.com>
2023-05-23 15:55:23 +0700

    update static mahasiswa

--- latihanstatic/Mahasiswa.java
+++ latihanstatic/Mahasiswa.java
@@ -6,6 +6,25 @@ public class Mahasiswa {
 
     public static void main(String[] args) {
         System.out.println(Mahasiswa.prodi);
-        System.out.println(Mahasiswa.nama);
+        //System.out.println(Mahasiswa.nama);
+
+        Mahasiswa st1 = new Mahasiswa();
+        System.out.println(st1.prodi);
+        System.out.println(st1.nama);
+
+        Mahasiswa st2 = new Mahasiswa();
+        System.out.println(st2.prodi);
+        System.out.println(st2.nama);
+
+        st1.prodi = "Teknologi RPL";
+        st1.nama = "Agus";
+        st2.prodi = "Teknologi Rekayasa Perangkat Lunak";
+        st2.nama = "Budi";
+
+        System.out.println(st1.prodi);
+        System.out.println(st1.nama);
+
+        System.out.println(st2.prodi);
+        System.out.println(st2.nama);
     }
 }
