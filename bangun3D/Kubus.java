b7ebc7c7dd788b2912fda77fcf70ab936121498a
Whyaziz <rizkyaziz214@gmail.com>
2023-05-23 15:31:20 +0700

    updated 5a

--- bangun3D/Kubus.java
+++ bangun3D/Kubus.java
@@ -1,4 +1,30 @@
 package bangun3D;
 
-public class Kubus {
+public class Kubus extends bangun2D.Persegi{
+    public Kubus(double sisi) {
+        super(sisi);
+    }
+
+    @Override
+    public double getLuas() {
+        return 6*super.getLuas();
+    }
+
+    @Override
+    public double getKeliling() {
+        return 12*super.getSisi();
+    }
+
+    public double getLuasAlas(){
+        return super.getLuas();
+    }
+
+    public double getVolume(){
+        return super.getLuas()*super.getSisi();
+    }
+
+    @Override
+    public String toString() {
+        return "Kubus{" + "sisi=" + getSisi() + '}';
+    }
 }
